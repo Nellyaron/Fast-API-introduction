4ab3ff0f43b841050997d9dd5b1705d77caf7ca3
Nellyaron <nelukegendabila@gmail.com>
2025-03-05 14:24:16 +0300

    Fastapi introduction

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Union
 
 from fastapi import FastAPI
 from model.schema import Item
+import uvicorn
+import os
 
 app = FastAPI()
 
@@ -20,3 +22,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Default to 8000 locally, use Render’s PORT in production
+    uvicorn.run(app, host="0.0.0.0", port=port)
